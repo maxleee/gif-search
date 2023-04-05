c1c5ca34c279c75904ba3f593467241cd298430c
Max <max.schwanger@gmail.com>
2023-04-05 08:24:05 -0400

    rough result grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SearchComponent } from './components/search/search.component';
+import { ResultGridComponent } from './components/result-grid/result-grid.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SearchComponent } from './components/search/search.component';
     HeaderComponent,
     FooterComponent,
     SearchComponent,
+    ResultGridComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
---
+++ src/app/components/result-grid/result-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-grid',
+  templateUrl: './result-grid.component.html',
+  styleUrls: ['./result-grid.component.scss']
+})
+export class ResultGridComponent {
+
+}
